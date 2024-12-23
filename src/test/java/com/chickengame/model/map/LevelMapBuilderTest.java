47e4f31f0df1ff8bc016256b40e4d15fbbe81e9e
Teresa Santos <teresafreitassantos@gmail.com>
2024-12-23 00:39:37 +0000

    Files added

---
+++ src/test/java/com/chickengame/model/map/LevelMapBuilderTest.java
@@ -0,0 +1,22 @@
+package com.chickengame.model.map;
+
+import com.chickengame.model.game.map.LevelMap;
+import net.jqwik.api.Property;
+import org.junit.jupiter.api.BeforeEach;
+/*
+public class LevelMapBuilderTest {
+    private LevelMap expectedLevelMap;
+
+    @BeforeEach
+    public void helper()
+    {
+
+    }
+
+    @Property
+    public void createLevelMap()
+    {
+
+    }
+}
+*/
