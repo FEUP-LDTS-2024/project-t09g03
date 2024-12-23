c2becd97fd14f946e7ceac17d4b970098ae90493
Teresa Santos <teresafreitassantos@gmail.com>
2024-12-23 20:35:52 +0000

    HelpControllerTest done

--- src/test/java/com/chickengame/controller/menus/GameEndControllerTest.java
+++ src/test/java/com/chickengame/controller/menus/GameEndControllerTest.java
@@ -22,21 +22,18 @@ public class GameEndControllerTest {
     private GUI gui;
     private GameEndController gameEndController;
     private GameEndMenu gameEndMenu;
-    private Button button;
 
     @BeforeEach
     public void helper() {
         this.gameEndMenu = Mockito.mock(GameEndMenu.class);
         this.game = Mockito.mock(Game.class);
         this.gui = Mockito.mock(GUI.class);
-        this.button = Mockito.mock(Button.class);
 
         this.gameEndController = new GameEndController(gameEndMenu);
     }
 
     @Test
     public void selectBack() {
-        Mockito.when(button.getType()).thenReturn(Button.Type.Back);
         gameEndController.step(game, gui, GUI.Action.SELECT);
 
         Mockito.verify(game, Mockito.times(1)).setState(any(MainMenuState.class));
